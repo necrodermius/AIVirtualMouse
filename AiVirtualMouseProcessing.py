82a32409fdb88fae402f1b69450ddb6131f27fb9
necrodermius <palach782@gmail.com>
2024-05-10 13:54:22 +0300

    code refactoring

--- AiVirtualMouseProcessing.py
+++ AiVirtualMouseProcessing.py
@@ -6,62 +6,92 @@ import autopy
 import pyautogui
 from settings import wCam, hCam, frameR, smoothening
 
+class AiVirtualMouse:
+    def __init__(self) -> None:
+        self.plocX = 0
+        self.plocY = 0
+        self.clocX = 0
+        self.clocY = 0
+        self.pTime = 0
+        self.cTime = 0
+        self.x1, self.y1, self.x2, self.y2 = None, None, None, None
 
-def FindFingers(cap, detector):
-    # 1. Find hand Landmarks
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList, bbox = detector.findPosition(img)
-    # 2. Get the tip of the index and middle fingers
-    if len(lmList) != 0:
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
-        # print(x1, y1, x2, y2)
-        return (img, x1, y1, x2, y2)
-    return (img, None, None, None, None)
+        self.cap = cv2.VideoCapture(1)
+        self.cap.set(3, wCam)
+        self.cap.set(4, hCam)
+        self.img = self.cap.read()
+        self.detector = htm.handDetector(maxHands=1)
+        self.fingers = []
+        self.lmList = []
+        self.is_pressed = False
+        
+        self.wScr, self.hScr = autopy.screen.size()
+        self.fps = None
+        
 
+    def FindFingers(self):
+        # 1. Find hand Landmarks
+        _, self.img = self.cap.read()
+        self.img = self.detector.findHands(self.img)
+        self.lmList, _ = self.detector.findPosition(self.img)
+        # 2. Get the tip of the index and middle fingers
+        if len(self.lmList) != 0:
+            self.x1, self.y1 = self.lmList[8][1:]
+            self.x2, self.y2 = self.lmList[12][1:]
+            # print(x1, y1, x2, y2)
 
-def CheckFingers(img, detector):
-    # 3. Check which fingers are up
-    fingers = detector.fingersUp()
-    # print(fingers)
-    cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
-    (255, 0, 255), 2)
-    return fingers
+    def CheckFingers(self):
+        # 3. Check which fingers are up
+        self.fingers = self.detector.fingersUp()
+        # print(fingers)
+        cv2.rectangle(self.img, (frameR, frameR), (wCam - frameR, hCam - frameR),
+        (255, 0, 255), 2)
+        
 
+    # 4. Only Index Finger : Moving Mode
+    def Moving(self):
+        # 5. Convert Coordinates
+        x3 = np.interp(self.x1, (frameR, wCam - frameR), (0, self.wScr))
+        y3 = np.interp(self.y1, (frameR, hCam - frameR), (0, self.hScr))
+        # 6. Smoothen Values
+        self.clocX = self.plocX + (x3 - self.plocX) / smoothening
+        self.clocY = self.plocY + (y3 - self.plocY) / smoothening
 
-# 4. Only Index Finger : Moving Mode
-def Moving(img, x1, y1, wScr, hScr, plocX, plocY):
-    # 5. Convert Coordinates
-    x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
-    y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
-    # 6. Smoothen Values
-    clocX = plocX + (x3 - plocX) / smoothening
-    clocY = plocY + (y3 - plocY) / smoothening
+        # 7. Move Mouse
+        autopy.mouse.move(self.wScr - self.clocX, self.clocY)
+        cv2.circle(self.img, (self.x1, self.y1), 15, (255, 0, 255), cv2.FILLED)
+        self.plocX, self.plocY = self.clocX, self.clocY
+        
 
-    # 7. Move Mouse
-    autopy.mouse.move(wScr - clocX, clocY)
-    cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-    plocX, plocY = clocX, clocY
-    return plocX, plocY, clocX, clocY
+    # 8. Both Index and middle fingers are up : Clicking Mode
+    def Clicking(self):
+        # 9. Find distance between fingers
+        length, self.img, lineInfo = self.detector.findDistance(8, 12, self.img)
+        # print(length)
+        # 10. Click mouse if distance short
+        if length < 30 and not self.is_pressed:
+            cv2.circle(self.img, (lineInfo[4], lineInfo[5]),
+            15, (0, 255, 0), cv2.FILLED)
+            autopy.mouse.click()
+        self.is_pressed = length < 30
 
-# 8. Both Index and middle fingers are up : Clicking Mode
-def Clicking(img, detector, is_pressed):
-    # 9. Find distance between fingers
-    length, img, lineInfo = detector.findDistance(8, 12, img)
-    print(length)
-    # 10. Click mouse if distance short
-    if length < 30 and not is_pressed:
-        cv2.circle(img, (lineInfo[4], lineInfo[5]),
-        15, (0, 255, 0), cv2.FILLED)
-        autopy.mouse.click()
-    is_pressed = length < 30
-    return is_pressed
-
-def FrameCalculating(img, pTime):
-    # 11. Frame Rate
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-    return cTime
+    def FrameCalculating(self):
+        # 11. Frame Rate
+        self.cTime = time.time()
+        self.fps = 1 / (self.cTime - self.pTime)
+        self.pTime = self.cTime
+        cv2.putText(self.img, str(int(self.fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        self.ptime = self.cTime
+    
+    def Run(self):
+        while True:
+            self.FindFingers()
+            self.CheckFingers()
+            if self.fingers[1] == 1 and self.fingers[2] == 0:
+                self.Moving()
+            if self.fingers[1] == 1 and self.fingers[2] == 1:
+                self.Clicking()
+            self.FrameCalculating()
+            # 12. Display
+            cv2.imshow("Image", self.img)
+            cv2.waitKey(1)
--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-import cv2
-import numpy as np
-import HandTrackingModule as htm
 import AiVirtualMouseProcessing as avmp
-import time
-import autopy 
-import pyautogui
-from settings import wCam, hCam, frameR, smoothening
 
-
-plocX, plocY = 0, 0
-clocX, clocY = 0, 0
-pTime = 0
-
-cap = cv2.VideoCapture(1)
-cap.set(3, wCam)
-cap.set(4, hCam)
-detector = htm.handDetector(maxHands=1)
-wScr, hScr = autopy.screen.size()
-
-is_pressed = False
-detector = htm.handDetector()
-
-while True:
-    img, x1, y1, x2, y2 = avmp.FindFingers(cap, detector)
-    fingers = avmp.CheckFingers(img, detector)
-    if fingers[1] == 1 and fingers[2] == 0:
-        plocX, plocY, clocX, clocY = avmp.Moving(img, x1, y1, wScr, hScr, plocX, plocY)
-    if fingers[1] == 1 and fingers[2] == 1:
-        is_pressed = avmp.Clicking(img, detector, is_pressed)
-    
-    pTime = avmp.FrameCalculating(img, pTime)
-    
-    # 12. Display
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+mouse = avmp.AiVirtualMouse()
+mouse.Run()
